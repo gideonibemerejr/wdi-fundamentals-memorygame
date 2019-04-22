3a6838e98c9b18f97d3d589ead663a4922e61ee8
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-04-21 20:21:28 -0500

    greated game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,1 +1,12 @@
-console.log("this is my cock");
+var cardOne = "queen";
+    cardTwo = "king";
+    cardThree= "queen";
+    cardFour = "king";
+
+
+
+
+    console.log("User flipped " + cardOne);
+    console.log("User flipped " + cardTwo);
+    console.log("User flipped " + cardThree);
+    console.log("User flipped " + cardFour);
