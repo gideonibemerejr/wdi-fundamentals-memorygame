13aa34c811c33a7d22d2c50dd0de6da083dd5e7e
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-04-22 00:19:45 -0500

    added more game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,1 +1,19 @@
 const cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+    } else {
+        alert("Sorry, try again.");
+    }
+}
+
+
+console.log("user flipped" + " " + cardsInPlay[0])
+console.log("user flipped" + " " + cardsInPlay[1])
