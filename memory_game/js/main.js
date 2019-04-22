852bb36e9b490856ae40a9309ac670c30dd2acc2
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-04-22 00:06:11 -0500

    adding js

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,12 +1,1 @@
-var cardOne = "queen";
-    cardTwo = "king";
-    cardThree= "queen";
-    cardFour = "king";
-
-
-
-
-    console.log("User flipped " + cardOne);
-    console.log("User flipped " + cardTwo);
-    console.log("User flipped " + cardThree);
-    console.log("User flipped " + cardFour);
+const cards = ["queen", "queen", "king", "king"];
