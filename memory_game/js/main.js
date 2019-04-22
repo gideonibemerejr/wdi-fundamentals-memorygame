28f23a2b2e663e37005520417ea2288cb90acd32
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-04-22 01:11:53 -0500

    created more game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,26 @@
 const cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert("You found a match!");
-    } else {
-        alert("Sorry, try again.");
+function checkForMatch() {
+    if (cardsInPlay.length === 2) {
+        //if the cards match, let the user know they've found a match
+        if (cardsInPlay[0] === cardsInPlay[1]) {
+            console.log("You found a match!");
+            //otherwise, let the user know they should try again
+        } else {
+            console.log("Sorry, try again.");
+        }
     }
 }
 
+function flipCard(cardId) {
+    //when a user flips over a card, add that card to the array of cards in play
+    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+    //if the user has flipped over two cards, check if they match
+    checkForMatch();
+}
 
-console.log("user flipped" + " " + cardsInPlay[0])
-console.log("user flipped" + " " + cardsInPlay[1])
+flipCard(0);
+flipCard(2);
+
